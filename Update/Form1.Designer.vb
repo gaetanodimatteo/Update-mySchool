50c0faaa94876e8a57af9018fd43126be265647c
Gaetano Di Matteo <gaetano.dimatteo@outlook.it>
2020-09-26 00:54:47 +0200

    Aggiungere i file di progetto.

---
+++ Update/Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla mediante l'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LV1 = New System.Windows.Forms.ListView()
+        Me.btnSostituisci = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.SuspendLayout()
+        '
+        'LV1
+        '
+        Me.LV1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LV1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2})
+        Me.LV1.HideSelection = False
+        Me.LV1.Location = New System.Drawing.Point(12, 60)
+        Me.LV1.Name = "LV1"
+        Me.LV1.Size = New System.Drawing.Size(578, 399)
+        Me.LV1.TabIndex = 0
+        Me.LV1.UseCompatibleStateImageBehavior = False
+        Me.LV1.View = System.Windows.Forms.View.Details
+        '
+        'btnSostituisci
+        '
+        Me.btnSostituisci.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnSostituisci.Location = New System.Drawing.Point(341, 482)
+        Me.btnSostituisci.Name = "btnSostituisci"
+        Me.btnSostituisci.Size = New System.Drawing.Size(249, 46)
+        Me.btnSostituisci.TabIndex = 1
+        Me.btnSostituisci.Text = "Sostituisci"
+        Me.btnSostituisci.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(271, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Posizionare i file da copiare nella cartella ""C:\mySchool"""
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Cartella"
+        Me.ColumnHeader1.Width = 241
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Esito"
+        Me.ColumnHeader2.Width = 109
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(602, 540)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnSostituisci)
+        Me.Controls.Add(Me.LV1)
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "Update"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LV1 As ListView
+    Friend WithEvents btnSostituisci As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+End Class
---
+++ Update/Form1.vb
@@ -0,0 +1,73 @@
+﻿Public Class Form1
+
+    Sub New()
+        InitializeComponent()
+    End Sub
+
+    Private Sub Sostituisci()
+        Dim ListaNon As New List(Of String)
+        Dim ListaSi As New List(Of String)
+        Dim PercorsoOrigine As String = LeggiValoreXML("percorsoorigine")
+        Dim Scuola As String = LeggiValoreXML("scuola")
+
+        If Scuola = "Assofram" Then
+            For Each Dir As String In System.IO.Directory.GetDirectories("c:\client")
+                If Dir.StartsWith("c:\client\cas") Then
+                    Try
+                        My.Computer.FileSystem.CopyFile(PercorsoOrigine & "\mySchool.exe", Dir & "\mySchool\mySchool.exe", True)
+                        My.Computer.FileSystem.CopyFile(PercorsoOrigine & "\ctrl.dll", Dir & "\mySchool\ctrl.dll", True)
+                        ListaSi.Add(Dir)
+                    Catch ex As Exception
+                        ListaNon.Add(Dir)
+                    End Try
+                ElseIf Dir.StartsWith("c:\client\Nap") Then
+                    Try
+                        My.Computer.FileSystem.CopyFile(PercorsoOrigine & "\mySchool.exe", Dir & "\mySchool.exe", True)
+                        My.Computer.FileSystem.CopyFile(PercorsoOrigine & "\ctrl.dll", Dir & "\ctrl.dll", True)
+                        ListaSi.Add(Dir)
+                    Catch ex As Exception
+                        ListaNon.Add(Dir)
+                    End Try
+                End If
+            Next
+        Else
+            For Each Dir As String In System.IO.Directory.GetDirectories("c:\")
+                If Dir.StartsWith("c:\mySchool") Then
+                    Try
+                        My.Computer.FileSystem.CopyFile(PercorsoOrigine & "\mySchool.exe", Dir & "\mySchool.exe", True)
+                        My.Computer.FileSystem.CopyFile(PercorsoOrigine & "\ctrl.dll", Dir & "\ctrl.dll", True)
+                        ListaSi.Add(Dir)
+                    Catch ex As Exception
+                        ListaNon.Add(Dir)
+                    End Try
+                End If
+            Next
+        End If
+
+        LV1.Items.Clear()
+        For Each s As String In ListaSi
+            LV1.Items.Add(s)
+            LV1.Items(LV1.Items.Count - 1).SubItems.Add("Completato")
+        Next
+        For Each s As String In ListaNon
+            LV1.Items.Add(s)
+            LV1.Items(LV1.Items.Count - 1).SubItems.Add("Sospeso")
+        Next
+    End Sub
+
+    Public Function LeggiValoreXML(ByVal Campo As String) As String
+        Try
+            Using ds As New DataSet
+                ds.ReadXml(Application.StartupPath & "\SettingUpdate.xml")
+                Return ds.Tables(0).Rows(0).Item(Campo).ToString
+            End Using
+        Catch ex As Exception
+            Return ""
+        End Try
+    End Function
+
+    Private Sub btnSostituisci_Click(sender As Object, e As EventArgs) Handles btnSostituisci.Click
+        Sostituisci()
+    End Sub
+
+End Class
